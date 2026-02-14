b5fc1b734fc5015f6a8ec85a5c0170b25cb144c1
Sameer Alsabea <sameeralsabea@Sameers-MacBook-Air.local>
2026-02-14 17:39:51 +0300

    enchance code readabilty and add blinking

--- main_example.py
+++ main_example.py
@@ -1,17 +1,25 @@
 from machine import Pin, SPI, I2C
 import time
 from Arducam import Arducam, JPEG, OV5642, ARDUCHIP_TRIG, CAP_DONE_MASK, ARDUCHIP_TIM
+import neopixel
 
+
+# Config the Pins and IDs
 SPI_ID   = 2
+
 SPI_SCK  = 12
 SPI_MOSI = 10
 SPI_MISO = 11
 CS_PIN   = 9
 
 I2C_ID   = 0
+
 I2C_SCL  = 14
 I2C_SDA  = 13
 
+led = neopixel.NeoPixel(Pin(38), 1)
+
+# Config the camera
 spi = SPI(SPI_ID, baudrate=2_000_000, polarity=0, phase=0,
           sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
 i2c = I2C(I2C_ID, scl=Pin(I2C_SCL), sda=Pin(I2C_SDA), freq=400_000)
@@ -28,10 +36,28 @@ cam.init()
 time.sleep(0.2)
 
 def try_capture(tag):
+    # Flush the cam FIFO buffer
     cam.flush_fifo()
     cam.clear_fifo_flag()
-    cam.start_capture()
+    
+    # blinking for 1 sec before start capturing
+    for _ in range(5):
+        led[0] = (255, 0, 0)  # Red
+        led.write()
+        time.sleep(0.1)
 
+        led[0] = (0, 0, 0)    # Off
+        led.write()
+        time.sleep(0.1)
+    
+    # Start Capturing
+    led[0] = (0, 255, 0)  # Green
+    led.write()
+    time.sleep(0.1)
+    
+    cam.start_capture()
+    
+    # Wait for 3s since the cam is asynchronized
     t0 = time.ticks_ms()
     while not cam.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK):
         if time.ticks_diff(time.ticks_ms(), t0) > 3000:
@@ -41,16 +67,28 @@ def try_capture(tag):
 
     length = cam.read_fifo_length()
     jpeg = cam.read_jpeg(max_size=None)
+    
+    # Print image detials
     print(tag, "TRIG:", hex(cam.Spi_read(ARDUCHIP_TRIG)[0]), "LEN:", length, "JPEG:", len(jpeg))
+    
+    # Finish Capturing
+    led[0] = (0, 0, 0)    # Off
+    led.write()
+    time.sleep(0.1)
+    
     return length, jpeg
 
-for tim in (0x00, 0x02):
-    cam.Spi_write(ARDUCHIP_TIM, tim)
-    time.sleep(0.05)
-    length, jpeg = try_capture("TIM=%s" % hex(tim))
-    if len(jpeg) > 1000:
-        with open("test.jpg", "wb") as f:
-            f.write(jpeg)
-        print("Saved test.jpg using", hex(tim))
-        break
+
+# Setting timing bit for the cam vsync as active-low (since active-high (0) didn't work)
+tim = 0x02 # 00000010
+cam.Spi_write(ARDUCHIP_TIM, tim)
+time.sleep(0.05)
+
+# Capture the image
+length, jpeg = try_capture("TIM=%s" % hex(tim))
+
+# Save image
+with open("test.jpg", "wb") as f:
+    f.write(jpeg)
+print("Saved test.jpg using", hex(tim))
 
